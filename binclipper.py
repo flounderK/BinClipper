3270769ce85bf004efaaea5c33af1a80b3f7c904
Clif Wolfe <clifwolfe1@gmail.com>
2021-06-09 22:45:55 -0400

    Added minimal replace funcitonality

--- binclipper.py
+++ binclipper.py
@@ -5,6 +5,17 @@ import re
 import sys
 import functools
 from abc import ABC, abstractmethod
+import binascii
+import struct
+import ctypes
+import io
+import logging
+
+log = logging.getLogger("BinClipper")
+_handler = logging.StreamHandler()
+_handler.setFormatter(logging.Formatter("%(levelname)-7s | %(message)s"))
+log.addHandler(_handler)
+log.setLevel(logging.WARNING)
 
 
 class BinMod(ABC):
@@ -16,11 +27,18 @@ class BinMod(ABC):
     def perform_binmod(self):
         pass
 
+    def write_to_stdout(self):
+        f = self.outpath
+        f.seek(0)
+        sys.stdout.buffer.write(f.read())
+
 
 class Clip(BinMod):
     def __init__(self, inpath, outpath, seek=0, number=-1, io_size=0x1000):
         self.inpath = inpath
-        self.outpath = outpath
+        outbuf = functools.partial(io.FileIO, outpath, "wb") if \
+                isinstance(outpath, str) else functools.partial(io.BytesIO)
+        self.outpath = outbuf()
         self.seek = seek
         self.number = number
         self.io_size = io_size
@@ -29,17 +47,71 @@ class Clip(BinMod):
         numread = 0
         readsize = self.io_size if self.number < 0 else self.number
         with open(self.inpath, "rb") as read_fd:
-            with open(self.outpath, "wb") as write_fd:
-                read_fd.seek(self.seek)
-                while read_fd.readable():
-                    rd = read_fd.read(readsize)
-                    numread += len(rd)
-                    write_fd.write(rd)
-                    if numread >= self.number:
-                        break
+            write_fd = self.outpath
+            read_fd.seek(self.seek)
+            while read_fd.readable():
+                rd = read_fd.read(readsize)
+                numread += len(rd)
+                write_fd.write(rd)
+                if numread >= self.number:
+                    break
 
             read_fd.close()
-            write_fd.close()
+
+
+class Replace(BinMod):
+    def __init__(self, inpath, outpath, replace_with_bytes, seek=0, number=-1):
+        assert isinstance(replace_with_bytes, bytes)
+        self.inpath = inpath
+        outbuf = functools.partial(io.FileIO, outpath, "wb") if \
+                isinstance(outpath, str) else functools.partial(io.BytesIO)
+        self.outpath = outbuf()
+        self.replace_with_bytes = replace_with_bytes
+        self.seek = seek
+        self.number = number if number != -1 else len(replace_with_bytes)
+
+    def perform_binmod(self):
+        with open(self.inpath, "rb") as read_fd:
+            write_fd = self.outpath
+            rd = read_fd.read(self.seek)
+            write_fd.write(rd)
+            write_fd.write(self.replace_with_bytes)
+            # current pos + number
+            read_fd.seek(self.number, 1)
+            write_fd.write(read_fd.read())
+            read_fd.close()
+
+
+def process_byte_input(input_mode, byte_input):
+    """Handle the byte input and output bytes"""
+    byte_output = None
+    ctypes_val = None
+    pack = struct.pack
+    if input_mode == 'hex':
+        return binascii.unhexlify(byte_input)
+
+    byte_input = int(byte_input, 0)
+    if input_mode == 'u8':
+        ctypes_val = ctypes.c_uint8(byte_input)
+    elif input_mode == 'u16':
+        ctypes_val = ctypes.c_uint16(byte_input)
+    elif input_mode == 'u32':
+        ctypes_val = ctypes.c_uint32(byte_input)
+    elif input_mode == 'u64':
+        ctypes_val = ctypes.c_uint64(byte_input)
+    else:
+        raise NotImplementedError("byte input mode not implemented")
+
+    if ctypes_val is not None:
+        byte_output = pack(ctypes_val._type_, ctypes_val.value)
+
+    return byte_output
+
+
+def validate_additional_arg_assertions(args):
+    if args.outpath is None and args.print is False:
+        raise Exception("Either outpath or print must be provided")
+        # raise argparse.ArgumentError("Either outpath or print must be provided")
 
 
 if __name__ == "__main__":
@@ -64,14 +136,17 @@ if __name__ == "__main__":
                                         help="Copy \"selected\" bytes to output")
     replace_parser = subparsers.add_parser("replace",
                                            help="Replace NUMBER bytes at the "
-                                           "offset of seek")
+                                           "offset of seek. If NUMBER is not provided "
+                                           "or is -1, the NUMBER of bytes replaced will "
+                                           "be set to the size of the replacing bytes")
     drop_parser = subparsers.add_parser("drop",
                                         help="Ignore \"selected\" bytes, "
                                         "the inverse of the \"clip\" command")
+    search_parser = subparsers.add_parser("search",
+                                          help="Just search for patterns")
 
-    BYTE_INPUT_MODES = ['hex', 'behex', 'lehex',
-                        'u8', 'u16', 'u32', 'u64',
-                        's8', 's16', 's32', 's64']
+    BYTE_INPUT_MODES = ['hex',
+                        'u8', 'u16', 'u32', 'u64']
     BYTE_INPUT_MODES_HELP = "Format of your input. By specifying one of the " \
                             "word options (u64 etc.) the size of your output is set " \
                             "to the size of that word type. All word options are " \
@@ -82,18 +157,29 @@ if __name__ == "__main__":
     replace_parser.add_argument("replace_with_bytes",
                                 help="The value that you are replacing the "
                                 "selected bytes with")
+
+    parser.add_argument("--debug", action="store_true", default=False)
     # set clip as the default behavior
     subparsers.default = "clip"
     # parser.add_argument("-v", "--invert", help="invert")
     args = parser.parse_args()
-    print(args)
-    if args.outpath is None and args.print is False:
-        raise argparse.ArgumentError("Either outpath or print must be provided")
+    if args.debug is True:
+        log.setLevel(logging.DEBUG)
+    log.debug(args)
+    validate_additional_arg_assertions(args)
+
+    if args.outpath is None:
+        args.outpath = io.BytesIO()
+
+    if args.subparser == 'replace':
+        # TODO: dirty, fix
+        args.replace_with_bytes = process_byte_input(args.replace_with_mode, args.replace_with_bytes)
 
     # dictionary mapping subparser name to class that will perform behavior
     subparser_handlers = {"clip": Clip,
-                          "replace": None,
-                          "drop": None}
+                          "replace": Replace,
+                          "drop": None,
+                          "search": None}
     # run handler
     handler_class = subparser_handlers.get(args.subparser)
     if handler_class is None:
@@ -103,7 +189,9 @@ if __name__ == "__main__":
     handler_args = handler_class.__init__.__code__.co_varnames
     used_args = {k: v for k, v in args._get_kwargs() if k in handler_args}
     handler = handler_class(**used_args)
-    print(handler)
     handler.perform_binmod()
+    if args.print is True:
+        handler.write_to_stdout()
+
 
 
