6f3749470ac00d7abc5fe0819133b05adead37e2
Clif Wolfe <clifwolfe1@gmail.com>
2021-06-11 16:36:52 -0400

    restructured binmod to support chains in the future and added basic search functionality

--- binclipper.py
+++ binclipper.py
@@ -29,58 +29,96 @@ class BinMod(ABC):
         pass
 
     def write_to_stdout(self):
-        f = self.outpath
+        f = self.outbuf
         f.seek(0)
         sys.stdout.buffer.write(f.read())
 
-    def close(self):
-        if hasattr(self.outpath, 'closed') and not self.outpath.closed:
-            self.outpath.close()
-        # if not self.inpath.closed:
-        #     self.inpath.close()
+    def open(self):
+        pass
 
+    def _get_file_like(self, inp, open_mode):
+        ret = None
+        if isinstance(inp, io.BytesIO) and not inp.closed:
+            ret = inp
+        elif isinstance(inp, str):
+            ret = io.FileIO(inp, open_mode)
+        else:
+            ret = io.BytesIO()
+
+        return ret
+
+    def _close_if_file(self, iofd):
+        if isinstance(iofd, io.FileIO) and iofd.closed is False:
+            iofd.close()
+
+    def close_all(self):
+        if hasattr(self.inbuf, 'closed') and self.inbuf.closed is False:
+            self.inbuf.close()
+        if hasattr(self.outbuf, 'closed') and self.outbuf.closed is False:
+            self.outbuf.close()
+
+    def close_files(self):
+        self._close_if_file(self.outbuf)
+        self._close_if_file(self.inbuf)
 
 
 class Clip(BinMod):
-    def __init__(self, inpath, outpath, seek=0, number=-1, io_size=0x1000):
-        self.inpath = inpath
-        outbuf = functools.partial(io.FileIO, outpath, "wb") if \
-                isinstance(outpath, str) else functools.partial(io.BytesIO)
-        self.outpath = outbuf()
+    def __init__(self, inpath, outpath, seek=0, number=-1):
+        self.inbuf = self._get_file_like(inpath, "rb")
+        self.outbuf = self._get_file_like(outpath, "wb")
         self.seek = seek
         self.number = number
-        self.io_size = io_size
 
     def perform_binmod(self):
-        with open(self.inpath, "rb") as read_fd:
-            write_fd = self.outpath
-            read_fd.seek(self.seek)
-            rd = read_fd.read(self.number)
-            write_fd.write(rd)
-            read_fd.close()
+        read_fd = self.inbuf
+        write_fd = self.outbuf
+        read_fd.seek(self.seek)
+        rd = read_fd.read(self.number)
+        write_fd.write(rd)
 
 
 class Replace(BinMod):
     def __init__(self, inpath, outpath, replace_with_bytes, seek=0, number=-1):
         assert isinstance(replace_with_bytes, bytes)
-        self.inpath = inpath
-        outbuf = functools.partial(io.FileIO, outpath, "wb") if \
-                isinstance(outpath, str) else functools.partial(io.BytesIO)
-        self.outpath = outbuf()
+        self.inbuf = self._get_file_like(inpath, "rb")
+        self.outbuf = self._get_file_like(outpath, "wb")
         self.replace_with_bytes = replace_with_bytes
         self.seek = seek
+        # default to the size of replace with bytes so that the size of the end file won't change
         self.number = number if number != -1 else len(replace_with_bytes)
 
     def perform_binmod(self):
-        with open(self.inpath, "rb") as read_fd:
-            write_fd = self.outpath
-            rd = read_fd.read(self.seek)
-            write_fd.write(rd)
-            write_fd.write(self.replace_with_bytes)
-            # current pos + number
-            read_fd.seek(self.number, 1)
-            write_fd.write(read_fd.read())
-            read_fd.close()
+        read_fd = self.inbuf
+        write_fd = self.outbuf
+        rd = read_fd.read(self.seek)
+        write_fd.write(rd)
+        write_fd.write(self.replace_with_bytes)
+        # current pos + number
+        read_fd.seek(self.number, 1)
+        write_fd.write(read_fd.read())
+
+# TODO: this probably shouldn't actually inherit from BinMod
+class Search(BinMod):
+    def __init__(self, inpath, outpath, search_for_bytes, seek=0, number=-1):
+        assert isinstance(search_for_bytes, bytes)
+        self.inbuf = self._get_file_like(inpath, "rb")
+        self.outbuf = None
+        self.search_for_bytes = search_for_bytes
+        self.seek = seek
+        self.number = number
+
+    def perform_binmod(self):
+        read_fd = self.inbuf
+        read_fd.seek(self.seek)
+        rd = read_fd.read()
+        escaped_search_for_bytes = re.escape(self.search_for_bytes)
+        for match in re.finditer(escaped_search_for_bytes, rd):
+            print("Found at offset %#x" % match.start())
+
+    def write_to_stdout(self):
+        pass
+
+
 
 
 def process_byte_input(input_mode, byte_input):
@@ -110,6 +148,7 @@ def process_byte_input(input_mode, byte_input):
 
 
 def validate_additional_arg_assertions(args):
+    """Things that argparse doesn't support out of the box"""
     if args.outpath is None and args.print is False:
         raise Exception("Either outpath or print must be provided")
         # raise argparse.ArgumentError("Either outpath or print must be provided")
@@ -164,28 +203,31 @@ if __name__ == "__main__":
                                 help="The value that you are replacing the "
                                 "selected bytes with")
 
+    search_parser.add_argument("byte_input_mode", choices=BYTE_INPUT_MODES,
+                               help=BYTE_INPUT_MODES_HELP)
+    search_parser.add_argument("input_bytes",
+                               help="The value that you are searching for")
+
     parser.add_argument("--debug", action="store_true", default=False)
     # set clip as the default behavior
     subparsers.default = "clip"
-    # parser.add_argument("-v", "--invert", help="invert")
     args = parser.parse_args()
     if args.debug is True:
         log.setLevel(logging.DEBUG)
     log.debug(args)
     validate_additional_arg_assertions(args)
 
-    if args.outpath is None:
-        args.outpath = io.BytesIO()
-
     if args.subparser == 'replace':
         # TODO: dirty, fix
         args.replace_with_bytes = process_byte_input(args.replace_with_mode, args.replace_with_bytes)
+    elif args.subparser == 'search':
+        args.search_for_bytes = process_byte_input(args.byte_input_mode, args.input_bytes)
 
     # dictionary mapping subparser name to class that will perform behavior
     subparser_handlers = {"clip": Clip,
                           "replace": Replace,
                           "drop": None,
-                          "search": None}
+                          "search": Search}
     # run handler
     handler_class = subparser_handlers.get(args.subparser)
     if handler_class is None:
@@ -198,7 +240,7 @@ if __name__ == "__main__":
     handler.perform_binmod()
     if args.print is True:
         handler.write_to_stdout()
-    handler.close()
+    handler.close_all()
 
 
 
