093197cdbf9d3a688f6f345fa3a4abbbcd7a19b2
Clif Wolfe <clifwolfe1@gmail.com>
2021-06-12 16:19:41 -0400

    Added some argparsing testcases

--- tests.py
+++ tests.py
@@ -15,9 +15,8 @@ class TestReplaceByOffset(unittest.TestCase):
         self.inpath.close()
         self.outpath.close()
 
-
     # TODO: add in printing a little bit of debug information if this fails
-    def test_replace_bytes_samesize(self):
+    def test_replace_bytes(self):
         byte_input = b'X'*random.randrange(0, 16)
         seek = random.randrange(0, len(self.inp))
         replacer = binclipper.Replace(self.inpath, self.outpath, byte_input, seek=seek)
@@ -27,6 +26,29 @@ class TestReplaceByOffset(unittest.TestCase):
         expected_byte_output = self.inp[:seek] + byte_input + self.inp[len(byte_input) + seek:]
         self.assertEqual(byte_output, expected_byte_output)
 
+    def test_replace_bytes_larger_than_input(self):
+        byte_input = b'X'*4
+        seek = 5
+        replacer = binclipper.Replace(self.inpath, self.outpath, byte_input, seek=seek, number=len(byte_input) + 3)
+        replacer.perform_binmod()
+        replacer.outbuf.seek(0)
+        byte_output = replacer.outbuf.read()
+        expected_byte_output = self.inp[:seek] + byte_input + self.inp[replacer.number + seek:]
+        self.assertEqual(byte_output, expected_byte_output)
+
+
+class TestArgumentParsing(unittest.TestCase):
+    def test_replace_args(self):
+        in_args = ['-s', '15', 'infile', '--print', 'replace', '64', '0x4444444444444444']
+        binclipper.parse_args(in_args)
+
+    def test_search_args(self):
+        in_args = ['infile', '--print', 'search', 'hex', '6c6c']
+        binclipper.parse_args(in_args)
+
+    def test_default_suparser_clip_args(self):
+        in_args = ['-s', '15', 'testshellcode', '--print']
+        binclipper.parse_args(in_args)
 
 
 
